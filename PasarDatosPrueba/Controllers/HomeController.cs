e4c83230e10a53c0a3a3a8619bd3366293c877a6
KhahoryITLA <20164247@itla.edu.do>
2019-06-01 15:28:07 -0400

    Version 1

---
+++ PasarDatosPrueba/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using PasarDatosPrueba.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PasarDatosPrueba.Controllers
+{
+    public class HomeController : Controller
+    {
+           
+        public ActionResult Index()
+        {
+            Persona persona = new Persona()
+            {
+                Nombre = "Angel"
+            };
+
+            //varible temporal el "nombre" seria como un id para identificar el tempData
+            TempData["nombre"] = persona.Nombre;
+            
+            return View(persona);
+        }
+
+        public ActionResult About()
+        {
+
+            //aqui solo llamo el contenido del tempData con el id "nombre"
+            string nombreVar = TempData["nombre"].ToString();
+
+            ViewBag.Message = nombreVar;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
